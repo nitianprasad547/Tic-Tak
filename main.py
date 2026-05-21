36ca696b9e10ad7675a084be5ed42915a8670c27
Kiran Kumar Pradhan <pradhankiran400@gmail.com>
2026-05-21 22:47:32 +0530

    chatbot added

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ import os
 import google.generativeai as genai
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
 
-genai.configure(api_key=os.getenv("GEMINI_API_KEY", "IzaSyBeY0CdVmyi2FKLDTfAd32DGI1oY6OyTCE"))
+# app = FastAPI() ke theek neeche yeh lagana padega:
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY", "AIzaSyAOCycIwgbvtjWIKQA7ckuQChQZ90P-1Tc"))
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash",
+    model_name="gemini-3.5-flash",
     system_instruction=(
         "You are a friendly assistant embedded in a Tic-Tac-Toe game. "
         "You can chat casually, answer questions about the game rules, "
